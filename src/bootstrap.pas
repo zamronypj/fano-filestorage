064d156f6ed324ef2a2ce755e6a23979fc308a57
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-01-19 08:47:41 +0800

    "Initial commit"

---
+++ src/bootstrap.pas
@@ -0,0 +1,57 @@
+(*!------------------------------------------------------------
+ * [[APP_NAME]] ([[APP_URL]])
+ *
+ * @link      [[APP_REPOSITORY_URL]]
+ * @copyright Copyright (c) [[COPYRIGHT_YEAR]] [[COPYRIGHT_HOLDER]]
+ * @license   [[LICENSE_URL]] ([[LICENSE]])
+ *------------------------------------------------------------- *)
+unit bootstrap;
+
+interface
+
+uses
+
+    fano;
+
+type
+
+    TAppServiceProvider = class(TDaemonAppServiceProvider)
+    protected
+
+    public
+        procedure register(const container : IDependencyContainer); override;
+    end;
+
+    TAppRoutes = class(TRouteBuilder)
+    public
+        procedure buildRoutes(
+            const container : IDependencyContainer;
+            const router : IRouter
+        ); override;
+    end;
+
+implementation
+
+uses
+    sysutils
+
+    (*! -------------------------------
+     *   controllers factory
+     *----------------------------------- *)
+    {---- put your controller factory here ---};
+
+
+
+    procedure TAppServiceProvider.register(const container : IDependencyContainer);
+    begin
+        {$INCLUDE Dependencies/dependencies.inc}
+    end;
+
+    procedure TAppRoutes.buildRoutes(
+        const container : IDependencyContainer;
+        const router : IRouter
+    );
+    begin
+        {$INCLUDE Routes/routes.inc}
+    end;
+end.
